6f4b626b4a3ca8dc7d487c2be196f92748db7f77
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-08-07 16:01:34 +0200

    Header ok

--- app/public/wp-content/themes/journey-turism-pro/patterns/header.php
+++ app/public/wp-content/themes/journey-turism-pro/patterns/header.php
@@ -8,25 +8,24 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"15px","top":"15px","left":"var:preset|spacing|80","right":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|30"}},"className":"menu","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
-<div class="wp-block-group alignfull menu" style="padding-top:15px;padding-right:var(--wp--preset--spacing--80);padding-bottom:15px;padding-left:var(--wp--preset--spacing--80)"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
+<div class="wp-block-group alignfull menu" style="padding-right:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
 <div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:image {"lightbox":{"enabled":false},"width":"231px","sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/JourneyTourism.png" alt="Logo JourneyTourism" style="width:231px"/></a></figure>
+<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/newlogoBleu.png" alt="Logo JourneyTourism" style="width:330px"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 <div class="wp-block-group" style="padding-right:0;padding-left:0"><!-- wp:navigation {"textColor":"custom-color-1","overlayTextColor":"custom-color-2","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}},"typography":{"textTransform":"capitalize","fontSize":"16px"}},"fontFamily":"tenor-sans"} -->
-<!-- wp:navigation-link {"label":"Blog","type":"","url":"/blog","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"Pêche","type":"","url":"/blog","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-submenu {"label":"Works","type":"","url":"#","kind":"custom"} -->
+<!-- wp:navigation-submenu {"label":"Restauration","type":"","url":"#","kind":"custom"} -->
 <!-- wp:navigation-link {"label":"Page One","type":"page","url":"#","kind":"custom","className":""} /-->
 <!-- /wp:navigation-submenu -->
 
-<!-- wp:navigation-link {"label":"Services","type":"","url":"/services","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"Notre produit","type":"","url":"/services","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-link {"label":"Destinations","type":"","url":"/destinations","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"Recettes","type":"","url":"/destinations","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-link {"label":"About Us","type":"","url":"/about","kind":"custom","isTopLevelLink":true} /-->
 
 <!-- wp:navigation-link {"label":"FAQ","type":"","url":"/faq","kind":"custom","isTopLevelLink":true} /-->
 <!-- /wp:navigation -->
